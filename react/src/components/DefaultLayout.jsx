cacd169d4dcb383aa987a238d52daeac45f6a9b9
Nithin Kumar K.S <nithinkzy@gmail.com>
2023-08-25 15:21:12 -0400

    Create Protected Routes

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout() {
+    const { user, token } = useStateContext();
+
+    if (!token) {
+        <Navigate to="/login" />;
+    }
     return (
         <div>
             <Outlet />
